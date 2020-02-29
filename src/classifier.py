d67ec8ce62d4e9f9c2d7e8e626d70e9f2d1515d8
peppocola <giuseppecolavito999@gmail.com>
2020-02-29 12:18:43 +0100

    add precision recall for both classes

--- src/classifier.py
+++ src/classifier.py
@@ -13,8 +13,24 @@ from src.plot_utils import saveHeatmap
 import time
 
 # TODO : add precision-recall curve and ROC ---> FOR FUN
+# TODO : create object stats
 metrics = [accuracy_score, average_precision_score, recall_score, precision_score, f1_score]
 metric_names = [x.__name__ for x in metrics]
+
+
+def custom_metrics(cm):
+    precision_no = str(round(cm[0][0] / (cm[0][0] + cm[0][1]), 2))
+    recall_no = str(round(cm[0][0] / (cm[0][0] + cm[1][0]), 2))
+    precision_yes = str(round(cm[1][1] / (cm[1][1] + cm[1][0]), 2))
+    recall_yes = str(round(cm[1][1] / (cm[1][1] + cm[0][1]), 2))
+    c_metrics = []
+    c_metrics.extend([(precision_no, 'precision_no'), (recall_no, 'recall_no'),
+                      (precision_yes, 'precision_yes'), (recall_yes, 'recall_yes')])
+    return c_metrics
+
+
+def custom_metrics_names():
+    return ['precision_no',  'recall_no', 'precision_yes', 'recall_yes']
 
 
 def classify(classifiers=None):
@@ -24,6 +40,8 @@ def classify(classifiers=None):
     labels = labelparser()  # the labels, or answers, we want to test against
 
     tfidf_vector = TfidfVectorizer(tokenizer=spacy_tokenizer)
+    for i in custom_metrics_names():
+        metric_names.append(i)
     stats = {key_classifier: [i.__name__ for i in classifiers], key_metric: metric_names[:]}
 
     for i in classifiers:
@@ -40,7 +58,9 @@ def classify(classifiers=None):
         stats[i.__name__] = []
         for metric in metrics:
             stats[i.__name__].append(str(round(metric(result, labels), 2)))
-
+        other_metrics = custom_metrics(cm)
+        for metric in other_metrics:
+            stats[i.__name__].append(metric[0])
     return stats
 
 
